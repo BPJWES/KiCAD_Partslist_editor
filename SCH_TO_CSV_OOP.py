59d40ec672b265a75e7c45a4edbb826ff730675f
BPJWES <krampalov@live.com>
2015-12-12 12:32:26 +0100

    Pulled changes from 6uhrmittag
    Fixed relative logo path issue, removed whitespace

--- SCH_TO_CSV_OOP.py
+++ SCH_TO_CSV_OOP.py
@@ -20,14 +20,14 @@ def OpenFile():
 			mainFile.setPath(filename)
 			data_test_dump = f.readlines()[0]
 			f.close()
-			
+
 		if data_test_dump[:-1] == "EESchema Schematic File Version 2":
 			#verify it conforms to KiCAD specs
 			if mainFile.getComponents():
 				#print(mainFile.getComponents())
 				#print("am deleting")
 				mainFile.deleteContents()
-				
+
 				#print(mainFile.getComponents())
 			try:
 				f = open(filename)
@@ -45,7 +45,7 @@ def OpenFile():
 	else:
 		if filename:
 			messagebox.showerror("FileParseError", "This is not a valid KiCAD schematic document.")
-	
+
 	for i in range (len(mainFile.getComponents())):
 		if "?" in mainFile.getComponents()[i].GetAnnotation():
 			messagebox.showerror("Annotation Incomplete", "The program is unable to process unanotated components")
@@ -62,33 +62,33 @@ def GenerateCSV():
 		messagebox.showerror("Cannot generate .CSV", "No SCH File loaded")
 def Break():
 	root.quit()
-	
+
 def listParts():
 	#print("test") this somehow get executed
 	#print("hoi")
 	if mainFile.get_number_of_components() != 0:
 		sub  = Tk()
 		height = mainFile.get_number_of_components()
-		
+
 		for i in range(height): #Rows
 				b = Entry(sub, text="")
 				b.grid(row=i, column=1)
 				b.insert(0,mainFile.getComponents()[i].GetAnnotation())
-				
+
 				c = Entry(sub, text="")
 				c.grid(row=i, column=2)
 				c.insert(0,mainFile.getComponents()[i].GetName())
-				
+
 				d = Entry(sub, text="")
 				d.grid(row=i, column=3)
 				d.insert(0,mainFile.getComponents()[i].GetFarnellLink())
-		
+
 def checklower(lowest_known, to_compare):
 	#returns 1 if lower
 	for i in range(len(lowest_known)):
 		if i  == len(to_compare):
 			return 1 #to compare is shorter i.e. lower
-			
+
 		if lowest_known[i] < to_compare[i]:
 			if lowest_known[i].isdigit() and to_compare[i].isdigit():
 				if len(to_compare) < len(lowest_known):
@@ -109,22 +109,22 @@ def sortList(this_list):
 	sortedList = []
 	lowest_known = "zzzzzzz"
 	position = 0
-	
+
 	for i in range(len(this_list)):
-	
+
 		for p in range(i,len(this_list)):
-		
+
 			if checklower(lowest_known,this_list[p]):
 				lowest_known = this_list[p]
 				position = p
-		
+
 		this_list[i] , this_list[position] = this_list[position] , this_list[i]
 		lowest_known = "zzzzzzz"
-		
+
 	sortList = this_list
-	
+
 	return sortedList
-	
+
 def sortParts():
 	mainFile.get_number_of_components()
 	componentNameList = []
@@ -135,7 +135,7 @@ def sortParts():
 		for p in range(i, mainFile.get_number_of_components()):
 			if componentNameList[i] == mainFile.getComponents()[p].GetAnnotation():
 				mainFile.SwapComponents(i,p)
-	
+
 def loadCSV():
 	#print("tbg")
 	root.filename = filedialog.askopenfilename(filetypes = (("KiCAD Partslist-editor files",".csv"),("All Files", ".*")))
@@ -154,19 +154,19 @@ def loadCSV():
 			#verify it conforms to KiCAD Partslist-editor specs
 			if openCSVFile.getComponents():
 				openCSVFile.deleteContents()
-				
+
 			f = open(filename)
-			
+
 			openCSVFile.setContents(f.readlines())
 			#openCSVFile.printContents()
 			#openCSVFile.printLine(1)
-			
+
 			openCSVFile.generateCSVComponents()
 			#openCSVFile.printComponents()
 			#for i in range (len(mainFile.getComponents())):
 			#	print(mainFile.getComponents()[i].GetAnnotation())
 			#mainFile.ModifyNewSCHFile(0, openCSVFile)
-			
+
 			f.close()
 			#mainFile.ParseComponents()
 		else:
@@ -203,7 +203,7 @@ root = Tk()
 root.configure(background='white')
 root.title("KiCAD Partslist-editor")
 root.bind("<Escape>", lambda e: e.widget.quit())
-background_image = PhotoImage(file="C:\\Users\\berjan\\Downloads\\KICAD.png")
+background_image = PhotoImage(file="KICAD_PLE.png")
 background = Label(root, image=background_image, bd=0)
 background.pack()
 
@@ -227,8 +227,5 @@ i.pack()
 #while 1:
 #	root.update()
 #	print("hoi")
-# this is supposed to enable interactive interface (show if 
+# this is supposed to enable interactive interface (show if
 mainloop()
-
-
-
