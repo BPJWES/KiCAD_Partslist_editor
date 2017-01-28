BPJWES/KiCAD_Partslist_editor

commit c6c35ed4d3b3eb25ecc56c0b64aa9e71400b36fe

Fixed issue with unparsed parsing of subcircuits, removed dead code

SCH_TO_CSV_OOP_LIB.py
@@ -52,7 +52,6 @@ class Component(object):
 		#parse the contents of a component for Fields
 		self.findLastFieldLine()
 		for anyField in self.fieldList:
-			print(anyField.name)
 			found = 0
 			for Alias in anyField.Aliases:
 				for line_nr in range(len(self.Contents)):
@@ -245,57 +244,20 @@ class SCH_FILE(object):
 									break
 						LastComponent.Value(content[count][startOfString:endOfString])			
 					
-					'''
-					if "FarnellLink" in content[count] or "farnelllink" in content[count] or "Farnelllink" in content[count] or "farnellLink" in content[count] : 
-						testvar = 0
-						
-						for i in range(len(content[count])):
-							if content[count][i] == "\"":
-								if testvar == 0:
-									startOfString = i+1
-									testvar = 1
-								else:
-									endOfString = i
-									break
-						LastComponent.SetFarnellLink(content[count][startOfString:endOfString])
-					if "MouserLink" in content[count] : 
-						testvar = 0
-						
-						for i in range(len(content[count])):
-							if content[count][i] == "\"":
-								if testvar == 0:
-									startOfString = i+1
-									testvar = 1
-								else:
-									endOfString = i
-									break
-						LastComponent.setMouserLink(content[count][startOfString:endOfString])
-					if "DigiKeyLink" in content[count] : 
-						testvar = 0
-						
-						for i in range(len(content[count])):
-							if content[count][i] == "\"":
-								if testvar == 0:
-									startOfString = i+1
-									testvar = 1
-								else:
-									endOfString = i
-									break
-						LastComponent.setDigiKeyLink(content[count][startOfString:endOfString])
-					
-						#ListOfFarnellLinks.append(content[count][startOfString:endOfString])
-					'''
 					count = count + 1
 				if not "#" in content[test_var+1]:
 					#ListOfFarnellLinks.append("")
+					
 					LastComponent.endpos(count)
 					LastComponent.Contents = content[LastComponent.startposition:LastComponent.endposition]
+					LastComponent.generateProperties()
+			#		print("parsed")
 				if test_var > 100000: # prevents fails
 					break
 		
 		for subcircuitcounter in range(len(self.subcircuits_names)):
 			
-			
+			#print("subcircuit")
 			for p in range (len(self.path)):
 				if self.path[-p] == "/":
 					break
@@ -310,6 +272,7 @@ class SCH_FILE(object):
 				self.get_subcircuit(subcircuitcounter).SetContents(f.readlines())
 				f.close()
 				self.get_subcircuit(subcircuitcounter).setSchematicName(self.subcircuits_names[subcircuitcounter])
+				self.get_subcircuit(subcircuitcounter).fieldList = self.fieldList
 				self.get_subcircuit(subcircuitcounter).ParseComponents()			
 				self.AppendComponents(self.get_subcircuit(subcircuitcounter).getComponents())
 	def get_subcircuit(self, x):
@@ -319,7 +282,7 @@ class SCH_FILE(object):
 		for item in range(len(componentList)):
 			self.components.append(componentList[item])
 			self.numb_of_comps = self.get_number_of_components() + 1
-	def SaveBOMInCSVNew(self,savepath):
+	def SaveBOMInCSV(self,savepath):
 	#New variant which allows for user configurable field names
 		if not '.csv' in savepath:
 			savepath = savepath + '.csv'
@@ -347,8 +310,6 @@ class SCH_FILE(object):
 				for field in self.fieldList:
 				#match fields to component.field
 					for counter in range(len(self.getComponents()[item].PropertyList)):
-						print(self.getComponents()[item].PropertyList[counter][0])
-						print(field.name)
 						if self.getComponents()[item].PropertyList[counter][0] == field.name:
 							f.write(self.getComponents()[item].PropertyList[counter][1])
 							f.write(",")
@@ -357,48 +318,6 @@ class SCH_FILE(object):
 						f.write("")
 						f.write(",")
 				f.write(self.getComponents()[item].GetSchematicName())
-				f.write("\n")
-			f.close
-	def SaveBOMInCSV(self,savepath):
-		if not '.csv' in savepath:
-			savepath = savepath + '.csv'
-		try:
-			f = open(savepath, 'w')
-		except IOError:
-			if savepath:	
-				return "error"
-		else:
-			f.write("Part\#")
-			f.write(",")
-			f.write("PartType")
-			f.write(",")
-			f.write("FarnellLink")
-			f.write(",")
-			f.write("MouserLink")
-			f.write(",")
-			f.write("DigiKeyLink")
-			f.write(",")
-			f.write("Found in: ")
-			f.write("\n")
-			for item in range(self.get_number_of_components()):#er stond -1
-				#print(str(item))
-				f.write(self.getComponents()[item].GetAnnotation())
-				f.write(",")
-				f.write(self.getComponents()[item].GetName())
-				f.write(",")
-				f.write(self.getComponents()[item].GetFarnellLink())
-				f.write(",")
-				f.write(self.getComponents()[item].getMouserLink())
-				f.write(",")
-				f.write(self.getComponents()[item].getDigiKeyLink())
-				f.write(",")
-				f.write(self.getComponents()[item].GetSchematicName())
-				f.write(",")#make conditional
-				f.write(str(self.getComponents()[item].getStartLine()))#make conditional
-				f.write(",")#make conditional
-				f.write(str(self.getComponents()[item].getEndLine()))#make conditional
-				f.write(",")
-				f.write(str(self.getComponents()[item].GetValue()))
 				f.write("\n")
 			f.close
 	def getSubCircuitName(self):

SCH_TO_CSV_OOP.py
@@ -112,7 +112,7 @@ def OpenFile():
 				mainFile.deleteContents()
 
 			break
-		mainFile.getComponents()[i].generateProperties()
+		#mainFile.getComponents()[i].generateProperties()
 	root.initialDirectory = setInitialDirectory(filename)
 	
 	
@@ -128,7 +128,7 @@ def GenerateCSV():
 		root.path_to_save = filedialog.asksaveasfilename(initialdir = initialDirectory, filetypes = (("Comma seperated values", ".csv"),("All Files",".*")))
 		root.initialDirectory = initialDirectory = setInitialDirectory(root.path_to_save)
 		sortParts()
-		if mainFile.SaveBOMInCSVNew(root.path_to_save):
+		if mainFile.SaveBOMInCSV(root.path_to_save):
 			messagebox.showerror("File IOerror", "The file might still be opened")
 	else:
 		messagebox.showerror("Cannot generate .CSV", "No SCH File loaded")

commit 97d819b7dfdc49a3f5de80b339af6ef07b1e585c

Fixed a bug in fileparser for reinserting new fields in .sch file
bug introduced in commit: c6c35ed4d3b3eb25ecc56c0b64aa9e71400b36fe

The lines which define field in the KiCad schematic fileformat were not
correctly generated.

Also fixed an issue caused by parsing "recovered schematic symbols"

SCH_TO_CSV_OOP_LIB.py
@@ -87,7 +87,7 @@ class Component(object):
 	def generatePropertyLine(self, property_nr):
 		cleanLine = getCleanLine(self.Contents[self.lastContentLine])
 		self.lastFieldLineNr = self.lastFieldLineNr+1
-		propertyString = cleanLine[:2] + str(self.lastFieldLineNr)+ cleanLine[3:5]+ self.PropertyList[property_nr][1] + cleanLine[5:-2] + self.PropertyList[property_nr][0] +"\""+ "\n"
+		propertyString = cleanLine[:2] + str(self.lastFieldLineNr)+ cleanLine[3:5]+ self.PropertyList[property_nr][1] + cleanLine[5:-1] + " \"" + self.PropertyList[property_nr][0] +"\""+ "\n"
 		return propertyString
 	def addNewInfo(self, CSV_propertylist):
 		for CSV_property in CSV_propertylist:	
@@ -124,8 +124,12 @@ def getCleanLine(line_to_be_cleaned):
 		#this line has been contaminated
 		line_to_be_returned = line_to_be_cleaned[:positions[0]+1] + line_to_be_cleaned[positions[1]:positions[2]+1] + line_to_be_cleaned[positions[3]:]
 	else :
-		#this line was clean
-		line_to_be_returned = line_to_be_cleaned
+		#this line was clean or there was a tilde sign in there
+		if "\"~\"" in line_to_be_cleaned:
+			#tilde is found in in kicad schematics with rescued symbols
+			line_to_be_returned = line_to_be_cleaned[:positions[0]+1] + line_to_be_cleaned[positions[1]:]
+		else:	
+			line_to_be_returned = line_to_be_cleaned
 	if "0000" in line_to_be_returned:
 		i = 0
 		for i in range (len(line_to_be_returned)-4):
@@ -134,6 +138,7 @@ def getCleanLine(line_to_be_cleaned):
 				break
 		
 		line_to_be_returned = line_to_be_returned[:i] + "0001" + line_to_be_returned[i+4 - len(line_to_be_returned):]
+	#print(line_to_be_returned)
 	return line_to_be_returned
 
 class SCH_FILE(object):
@@ -323,7 +328,9 @@ class SCH_FILE(object):
 	def getSubCircuits(self):
 		return self.subcircuits
 	def ModifyNewSCHFile(self, oldSCHFile, CSV_FILE, savepath):
-		#this will break if the order is not FarnellLink; MouserLink; DigiKeyLink
+		# this did break if the order is not FarnellLink; MouserLink; DigiKeyLink
+		# should be fixed now but am not sure
+		
 		print(str(CSV_FILE.getNumberOfComponents()))
 		print(str(self.get_number_of_components()))
 		
@@ -357,7 +364,7 @@ class SCH_FILE(object):
 			for i in range(len(self.subcircuits)):
 				for p in range (len(savepath)):
 					if savepath[-p] == "/":
-						break #find first forwardslash to add other file name
+						break #find first forward slash to add other file name
 				
 				new_savepath = savepath[:-p+1]+self.subcircuits_names[i]
 				print("new_savepath")