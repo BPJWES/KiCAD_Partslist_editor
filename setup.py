9f0ce3acff48a903a07d3a22818af1874cb253ad
BPJWES <bpj.westerdijk@gmail.com>
2018-05-02 12:35:03 +0200

    cleaned up config file, rebuild binary for new version

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from cx_Freeze import setup, Executable
+import os
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\berjan\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\berjan\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
 
 setup(name = 'KiCAD_PLE',
-		version = '18.0.1',
+		version = '18.0.2',
 		description = 'Partslist Editor',
 		executables = [Executable("PartsListEditor.py")])
+	
